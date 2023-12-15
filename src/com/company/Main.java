5b0aee9b7d978cfe12e1d96d135b3e82a24eb983
TimofeyShved <timofeya123@inbox.ru>
2023-12-15 22:09:37 +0600

    new maill librarian

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Перед началом работы необходимо скачать библиотеку javax.mail.jar
+        // Ссылка на него https://javaee.github.io/javamail/
+        // Далее подключить (File -> Project Structure -> Librarian)
+
+        
     }
 }
